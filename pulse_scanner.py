ff6722cf2114998e1f10699031b9b6c6e5e3175b
Vedant Agarwal <vedantagarwal4@gmail.com>
2020-08-07 23:20:12 +0530

    function for numeric_val

--- pulse_scanner.py
+++ pulse_scanner.py
@@ -46,3 +46,51 @@ def string_val(source_code, i, table, line_num, start_char='"'):
 
     # Return string token and current index in source code
     return Token("string", id, line_num), i
+
+def numeric_val(source_code, i, table, line_num):
+    """
+    Processes numeric values in the source code
+    Params
+    ======
+    source_code (string)      = The string containing pulse source code
+    i           (int)         = The current index in the source code
+    table       (SymbolTable) = Symbol table constructed holding information about identifiers and constants
+    line_num    (int)         = Line number
+    Returns
+    =======
+    Token, int: The token generated for the numeric constant and the current position in source code,
+                this is done only if there is no error in the numeric constant
+    """
+
+    numeric_constant = ""
+
+    # Loop until we get a non-digit character
+    while is_digit(source_code[i]):
+        numeric_constant += source_code[i]
+        i += 1
+
+    # If a numeric constant contains more than 1 decimal point (.) then that is invalid
+    if numeric_constant.count(".") > 1:
+        error(
+            "Invalid numeric constant, cannot have more than one decimal point in a"
+            " number!" , line_num
+        )
+
+    # Check the length after . to distinguish between float and double
+    length = len(numeric_constant.split(".")[1]) if "." in numeric_constant else 0
+
+    # Determine type of numeric value
+    type = "int"
+    if length != 0:
+        if length <= 7:
+            type = "float"
+        elif length >= 7:
+            type = "double"
+
+    # Make entry in symbol table
+    id = table.entry(numeric_constant, type, "constant")
+
+    # Return number token and current index in source code
+    return Token("number", id, line_num), i
+
+
