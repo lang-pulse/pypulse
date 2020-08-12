70abda6d469fc012879a58eac459d59d13565554
frankhart2018 <siddharthadhar_soumen@srmuniv.edu.in>
2020-08-12 17:48:09 +0530

    Clear out unnecessary code and fix comments, docstrings

--- pulse_scanner.py
+++ pulse_scanner.py
@@ -16,11 +16,14 @@ def is_keyword(value):
     Checks if string is keyword or not
     Params
     ======
-    value (string) = The string to be checked for keyword
+    value (str)
+        : The string to be checked for keyword
     Returns
     =======
-    bool: Whether the value passed is a keyword or not
+    (bool)
+        : Whether the value passed is a keyword or not
     """
+
     return value in [
         "and",
         "or",
@@ -41,20 +44,27 @@ def is_keyword(value):
     ]
 
 
-def keyword_identifier(source_code, i, table, line_num, scanner_obj):
+def keyword_identifier(source_code, i, table, scanner_obj):
     """
     Process keywords and identifiers in source code
     Params
     ======
-    source_code (string) = The string containing simc source code
-    i           (int)    = The current index in the source code
-    table       (SymbolTable) = Symbol table constructed holding information about identifiers and constants
-    line_num    (int)         = Line number
+    source_code (str)
+        : The string containing pulse source code
+    i           (int)
+        : The current index in the source code
+    table       (SymbolTable)
+        : Symbol table constructed holding information about identifiers and constants
+    scanner_obj (Scanner)
+        : Instance of Scanner class
     Returns
     =======
-    Token, int: The token generated for the keyword or identifier and the current position in source code
+    (Token)
+        : The token generated for the keyword or identifier
+    (int)
+        : Current position in source code
     """
-    # an empty string is assigned to value"
+
     value = ""
 
     # Loop until we get a non-digit character
@@ -64,7 +74,7 @@ def keyword_identifier(source_code, i, table, line_num, scanner_obj):
 
     # Check if value is keyword or not
     if is_keyword(value):
-        return Token(value, "", line_num), i
+        return Token(value, "", scanner_obj.line_num), i
 
     # Check if identifier is in symbol table
     id = table.get_by_symbol(value)
@@ -74,22 +84,30 @@ def keyword_identifier(source_code, i, table, line_num, scanner_obj):
         id = table.entry(value, "var", "variable")
 
     # Returns the  id, token and current index in source code
-    return Token("id", id, line_num), i
+    return Token("id", id, scanner_obj.line_num), i
 
 
-def string_val(source_code, i, table, line_num, scanner_obj, start_char='"'):
+def string_val(source_code, i, table, scanner_obj, start_char='"'):
     """
     Processes string values in the source code
     Params
     ======
-    source_code (string) = The string containing simc source code
-    i           (int)    = The current index in the source code
-    table       (SymbolTable) = Symbol table constructed holding information about identifiers and constants
-    line_num    (int)         = Line number
+    source_code (str)
+        : The string containing simc source code
+    i           (int)
+        : The current index in the source code
+    table       (SymbolTable)
+        : Symbol table constructed holding information about identifiers and constants
+    scanner_obj (Scanner)
+        : Instance of Scanner class
+    start_char  (str) (Optional)
+        : Character with which string starts
     Returns
     =======
-    Token, int: The token generated for the string constant and the current position in source code,
-                this is done only if there is no error in the string constant
+    (Token)
+        : The token generated for the string constant
+    (int)
+        : Current position in source code
     """
 
     string_constant = ""
@@ -100,7 +118,7 @@ def string_val(source_code, i, table, line_num, scanner_obj, start_char='"'):
     # Loop until we get a non-digit character
     while source_code[i] != start_char:
         if source_code[i] == "\0":
-            error("Unterminated string!", line_num)
+            error("Unterminated string!", scanner_obj.line_num)
 
         string_constant += source_code[i]
         i += 1
@@ -115,22 +133,28 @@ def string_val(source_code, i, table, line_num, scanner_obj, start_char='"'):
     id = table.entry(string_constant, "string", "constant")
 
     # Return string token and current index in source code
-    return Token("string", id, line_num), i
+    return Token("string", id, scanner_obj.line_num), i
 
 
-def numeric_val(source_code, i, table, line_num, scanner_obj):
+def numeric_val(source_code, i, table, scanner_obj):
     """
     Processes numeric values in the source code
     Params
     ======
-    source_code (string)      = The string containing pulse source code
-    i           (int)         = The current index in the source code
-    table       (SymbolTable) = Symbol table constructed holding information about identifiers and constants
-    line_num    (int)         = Line number
+    source_code (str)
+        : The string containing simc source code
+    i           (int)
+        : The current index in the source code
+    table       (SymbolTable)
+        : Symbol table constructed holding information about identifiers and constants
+    scanner_obj (Scanner)
+        : Instance of Scanner class
     Returns
     =======
-    Token, int: The token generated for the numeric constant and the current position in source code,
-                this is done only if there is no error in the numeric constant
+    (Token)
+        : The token generated for the numeric constant
+    (int)
+        : Current position in source code
     """
 
     numeric_constant = ""
@@ -145,7 +169,7 @@ def numeric_val(source_code, i, table, line_num, scanner_obj):
         error(
             "Invalid numeric constant, cannot have more than one decimal point in a"
             " number!",
-            line_num,
+            scanner_obj.line_num,
         )
 
     # Check the length after . to distinguish between float and double
@@ -163,24 +187,31 @@ def numeric_val(source_code, i, table, line_num, scanner_obj):
     id = table.entry(numeric_constant, type, "constant")
 
     # Return number token and current index in source code
-    return Token("number", id, line_num), i
+    return Token("number", id, scanner_obj.line_num), i
 
 
-def checkUnindent(source_code, i, table, line_num, scanner_obj):
+def check_unindent(source_code, i, table, scanner_obj):
     """
-    processes indentation in the source code
+    Processes indentation in the source code
     Params
     ======
-    source_code (string) = The string containing simc source code
-    i           (int)    = The current index in the source code
-    table       (SymbolTable) = Symbol table constructed holding information about identifiers and constants
-    line_num    (int)         = Line number
+    source_code (str)
+        : The string containing pulse source code
+    i           (int)
+        : The current index in the source code
+    table       (SymbolTable)
+        : Symbol table constructed holding information about identifiers and constants
+    scanner_obj (Scanner)
+        : Instance of Scanner class
     Returns
     =======
-    int: the current position in source code
+    (int)
+        : the current position in source code
     """
-    # checks if the code is indented following ':'
+
+    # Checks if the code is indented following ':'
     if scanner_obj.isIndent:
+        # Count spaces/tabs
         localTabCount = 0
         localSpaceCount = 0
         while source_code[i] == "\t":
@@ -190,12 +221,15 @@ def checkUnindent(source_code, i, table, line_num, scanner_obj):
             localSpaceCount += 1
             i += 1
 
+        # Convert spaces into tabs
         localTabCount = localSpaceCount // 2 if localTabCount == 0 else localTabCount
 
+        # If the number of tabs before a line are less than the current level of indentation then setup unindentation
         if localTabCount < scanner_obj.indentLevel:
             scanner_obj.isUnindent = True
             scanner_obj.unindentLevel = scanner_obj.indentLevel - localTabCount
 
+        # If scanner's indentation level is zero then set isIndent to false
         if scanner_obj.indentLevel == 0:
             scanner_obj.isIndent = False
 
@@ -203,6 +237,13 @@ def checkUnindent(source_code, i, table, line_num, scanner_obj):
 
 
 def gen_unindent(scanner_obj):
+    """
+    Generates unindent token
+    Params
+    ======
+    scanner_obj (Scanner)
+        : Instance of Scanner class
+    """
 
     if scanner_obj.unindentLevel > 0:
         print("Here-", scanner_obj.indentLevel)
@@ -221,12 +262,13 @@ def scanner(source_code, table):
     Generate tokens from source code
     Params
     ======
-    filename    (string)      = The string containing pulse source code filename
-    table       (SymbolTable) = Symbol table constructed holding information about identifiers and constants
+    source_code (str)
+        : Pulse source code
+    table       (SymbolTable)
+        : Symbol table constructed holding information about identifiers and constants
     Returns
     ========
-    tokens: A list of tokens of the source code, if the code is lexically correct, otherwise
-            presents user with an error
+    tokens: A list of tokens of the source code
     """
 
     # Create scanner_obj class' object
@@ -242,31 +284,25 @@ def scanner(source_code, table):
         # If a digit appears, call numeric_val function and add the numeric token to list,
         # if it was correct
         if is_digit(source_code[i]):
-            token, i = numeric_val(
-                source_code, i, table, scanner_obj.line_num, scanner_obj
-            )
+            token, i = numeric_val(source_code, i, table, scanner_obj)
             scanner_obj.tokens.append(token)
 
-        # If quote appears the value is a string token
+        # If double quote appears the value is a string token
         elif source_code[i] == '"':
-            token, i = string_val(
-                source_code, i, table, scanner_obj.line_num, scanner_obj
-            )
+            token, i = string_val(source_code, i, table, scanner_obj)
             scanner_obj.tokens.append(token)
 
+        # If single quote appears the value is a string token
         elif source_code[i] == "'":
-            token, i = string_val(
-                source_code, i, table, scanner_obj.line_num, scanner_obj, "'"
-            )
+            token, i = string_val(source_code, i, table, scanner_obj, "'")
             scanner_obj.tokens.append(token)
 
         # If alphabet or number appears then it might be either a keyword or an identifier
         elif is_alnum(source_code[i]):
-            token, i = keyword_identifier(
-                source_code, i, table, scanner_obj.line_num, scanner_obj
-            )
+            token, i = keyword_identifier(source_code, i, table, scanner_obj)
             scanner_obj.tokens.append(token)
 
+        # If character is : then generate begin block token and start indentation
         elif source_code[i] == ":":
             token = Token("begin_block", "", scanner_obj.line_num)
             scanner_obj.tokens.append(token)
@@ -274,19 +310,20 @@ def scanner(source_code, table):
             scanner_obj.indentLevel += 1
             i += 1
 
+        # If character is \n then generate newline token and check for unindentation
         elif source_code[i] == "\n":
             scanner_obj.line_num += 1
             token = Token("newline", "", scanner_obj.line_num)
             scanner_obj.tokens.append(token)
-            i = checkUnindent(
-                source_code, i + 1, table, scanner_obj.line_num, scanner_obj
-            )
+            i = check_unindent(source_code, i + 1, table, scanner_obj)
             gen_unindent(scanner_obj)
 
+        # If character is ( then generate left paren token
         elif source_code[i] == "(":
             scanner_obj.tokens.append(Token("left_paren", "", scanner_obj.line_num))
             i += 1
 
+        # If character is ) then generate right paren token
         elif source_code[i] == ")":
             scanner_obj.tokens.append(Token("right_paren", "", scanner_obj.line_num))
             i += 1
@@ -309,7 +346,8 @@ def scanner(source_code, table):
             else:
                 scanner_obj.tokens.append(Token("assignment", "", scanner_obj.line_num))
                 i += 1
-        # Identifying plus_equal, increment or plus token
+
+        # Identifying plus equal, increment or plus token
         elif source_code[i] == "+":
             if source_code[i + 1] == "=":
                 scanner_obj.tokens.append(Token("plus_equal", "", scanner_obj.line_num))
@@ -321,7 +359,7 @@ def scanner(source_code, table):
                 scanner_obj.tokens.append(Token("plus", "", scanner_obj.line_num))
                 i += 1
 
-        # Identifying minus_equal, decrement or minus token
+        # Identifying minus equal, decrement or minus token
         elif source_code[i] == "-":
             if source_code[i + 1] == "=":
                 scanner_obj.tokens.append(
@@ -335,7 +373,7 @@ def scanner(source_code, table):
                 scanner_obj.tokens.append(Token("minus", "", scanner_obj.line_num))
                 i += 1
 
-        # Identifying multiply_equal or multiply token
+        # Identifying multiply equal or multiply token
         elif source_code[i] == "*":
             if source_code[i + 1] == "=":
                 scanner_obj.tokens.append(
@@ -346,7 +384,7 @@ def scanner(source_code, table):
                 scanner_obj.tokens.append(Token("multiply", "", scanner_obj.line_num))
                 i += 1
 
-        # Identifying single_line_comment token
+        # Identifying single line comment token
         elif source_code[i] == "#":
             i += 1
             while source_code[i] != "\n":
@@ -357,7 +395,7 @@ def scanner(source_code, table):
             )
             comment_str = ""
 
-        # Identifying multi_line_comment, divide_equal,integer_divide, divide token
+        # Identifying multi line comment, divide_equal, integer_divide, divide token
         elif source_code[i] == "/":
             if source_code[i + 1] == "*":
                 i += 2
@@ -383,7 +421,7 @@ def scanner(source_code, table):
                 scanner_obj.tokens.append(Token("divide", "", scanner_obj.line_num))
                 i += 1
 
-        # Identifying modulus_equal or modulus token
+        # Identifying modulus equal or modulus token
         elif source_code[i] == "%":
             if source_code[i + 1] == "=":
                 scanner_obj.tokens.append(
@@ -404,7 +442,7 @@ def scanner(source_code, table):
             scanner_obj.tokens.append(Token("not_equal", "", scanner_obj.line_num))
             i += 2
 
-        # Identifying greater_than or greater_than_equal token
+        # Identifying greater_than or greater than equal token
         elif source_code[i] == ">":
             if source_code[i + 1] == "=":
                 scanner_obj.tokens.append(
@@ -417,7 +455,7 @@ def scanner(source_code, table):
                 )
                 i += 1
 
-        # Identifying less_than or less_than_equal to token
+        # Identifying less than or less than equal to token
         elif source_code[i] == "<":
             if source_code[i + 1] == "=":
                 scanner_obj.tokens.append(
@@ -428,14 +466,14 @@ def scanner(source_code, table):
                 scanner_obj.tokens.append(Token("less_than", "", scanner_obj.line_num))
                 i += 1
 
-        # Identifying the token_left_bracket
+        # Identifying the token left_bracket
         elif source_code[i] == "[":
             scanner_obj.tokens.append(
                 Token("token_left_bracket", "", scanner_obj.line_num)
             )
             i += 1
 
-        # Identifying the token_right_bracket
+        # Identifying the token right_bracket
         elif source_code[i] == "]":
             scanner_obj.tokens.append(
                 Token("token_right_bracket", "", scanner_obj.line_num)
@@ -446,6 +484,7 @@ def scanner(source_code, table):
         else:
             i += 1
 
+    # If indentLevel is not 0 then generate unindent tokens until indentLevel is zero
     if scanner_obj.indentLevel > 0:
         while scanner_obj.indentLevel != 0:
             token = Token("unindent", "", scanner_obj.line_num)
